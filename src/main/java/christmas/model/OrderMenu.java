4e501bae79d74f35f6b393b95404b2d582c35b55
LEE-sh1673 <lsh901673@gmail.com>
2023-11-11 22:09:08 +0900

    refactor: 클래스 책임 분리
    메뉴 형식에 대한 검증 기능을 OrderMenu가 담당하도록 수정하였습니다.

--- src/main/java/christmas/model/OrderMenu.java
+++ src/main/java/christmas/model/OrderMenu.java
@@ -3,7 +3,12 @@ package christmas.model;
 import static christmas.exception.ErrorType.INVALID_ORDER;
 import static christmas.exception.ErrorType.MAX_ORDER_EXCEEDED;
 
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class OrderMenu {
+
+    private static final Pattern MENU_PATTERN = Pattern.compile("\\S+-[0-9]+");
 
     private static final int MIN_ORDER_QUANTITY = 1;
     private static final int MAX_ORDER_QUANTITY = 20;
@@ -12,10 +17,18 @@ public class OrderMenu {
 
     private final int quantity;
 
-    public OrderMenu(final String menuName, final int quantity) {
+    public OrderMenu(final String nameWithQuantity) {
+        validateFormat(nameWithQuantity);
+        final String[] menuInfo = nameWithQuantity.split("-");
+        this.menu = Menu.findMenu(menuInfo[0]);
+        this.quantity = Integer.parseInt(menuInfo[1]);
         validate(quantity);
-        this.menu = Menu.findMenu(menuName);
-        this.quantity = quantity;
+    }
+
+    private void validateFormat(final String name) {
+        if (name == null || !MENU_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
     }
 
     private void validate(final long quantity) {
@@ -37,5 +50,22 @@ public class OrderMenu {
 
     public long getPrize() {
         return menu.getPrize() * quantity;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final OrderMenu other = (OrderMenu) o;
+        return menu == other.menu;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menu);
     }
 }
--- src/test/java/christmas/model/OrderMenuTest.java
+++ src/test/java/christmas/model/OrderMenuTest.java
@@ -8,13 +8,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderMenuTest {
 
     @DisplayName("메뉴의 수량이 1 이상의 숫자가 아닌 경우 예외가 발생한다.")
     @Test
     void givenQuantityLessThanOne_Then_ExceptionOccurs() {
-        assertThatThrownBy(() -> new OrderMenu("티본스테이크", 0))
+        assertThatThrownBy(() -> new OrderMenu("티본스테이크-0"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorType.INVALID_ORDER.getMessage());
     }
@@ -22,22 +24,31 @@ class OrderMenuTest {
     @DisplayName("메뉴의 수량이 20개를 초과하는 경우 예외가 발생한다.")
     @Test
     void givenQuantityExceeded_Then_ExceptionOccurs() {
-        assertThatThrownBy(() -> new OrderMenu("티본스테이크", 21))
+        assertThatThrownBy(() -> new OrderMenu("티본스테이크-21"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorType.MAX_ORDER_EXCEEDED.getMessage());
+    }
+
+    @DisplayName("메뉴의 형식이 '메뉴-개수' 형식이 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = {"티본-티본", "티본-비본-1", "  -1", "    타파스-20", "티본스테이크-20   "})
+    void givenInvalidMenuForm_Then_ExceptionOccurs(final String menuName) {
+        assertThatThrownBy(() -> new OrderMenu(menuName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorType.INVALID_ORDER.getMessage());
     }
 
     @DisplayName("주문 메뉴의 가격을 구한다.")
     @ParameterizedTest
     @CsvSource(value = {
-            "티본스테이크:1:55000", "바비큐립:2:108000",
-            "초코케이크:2:30000", "제로콜라:1:3000"}, delimiter = ':')
+            "티본스테이크-1:55000", "바비큐립-2:108000",
+            "초코케이크-2:30000", "제로콜라-1:3000"}, delimiter = ':')
     void givenOrderMenu_Then_priceReturns(
-            final String menuName,
-            final int quantity,
+            final String nameWithQuantity,
             final long price
     ) {
-        final OrderMenu orderMenu = new OrderMenu(menuName, quantity);
+        final OrderMenu orderMenu = new OrderMenu(nameWithQuantity);
         assertThat(orderMenu.getPrize()).isEqualTo(price);
     }
 }
