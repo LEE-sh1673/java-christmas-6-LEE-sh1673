edeb6438316d3000f6ca6d2a5002a5f0ecec5ad1
LEE-sh1673 <lsh901673@gmail.com>
2023-11-11 21:09:04 +0900

    test: 주문 메뉴의 가격을 구하는 테스트 추가

--- src/test/java/christmas/model/OrderMenuTest.java
+++ src/test/java/christmas/model/OrderMenuTest.java
@@ -1,10 +1,13 @@
 package christmas.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.exception.ErrorType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class OrderMenuTest {
 
@@ -22,5 +25,19 @@ class OrderMenuTest {
         assertThatThrownBy(() -> new OrderMenu("티본스테이크", 21))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorType.MAX_ORDER_EXCEEDED.getMessage());
+    }
+
+    @DisplayName("주문 메뉴의 가격을 구한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "티본스테이크:1:55000", "바비큐립:2:108000",
+            "초코케이크:2:30000", "제로콜라:1:3000"}, delimiter = ':')
+    void givenOrderMenu_Then_priceReturns(
+            final String menuName,
+            final int quantity,
+            final long price
+    ) {
+        final OrderMenu orderMenu = new OrderMenu(menuName, quantity);
+        assertThat(orderMenu.getPrize()).isEqualTo(price);
     }
 }
