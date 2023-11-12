a0300311376304c9677a4e259376fd6d375da8ad
LEE-sh1673 <lsh901673@gmail.com>
2023-11-12 18:04:23 +0900

    feat: 주문 일자의 범위에 대한 검증 기능 추가
    1 이상 31 이하의 숫자가 아닌 경우 예외가 발생한다.

---
+++ src/main/java/christmas/model/OrderDate.java
@@ -0,0 +1,26 @@
+package christmas.model;
+
+import static christmas.exception.ErrorType.INVALID_DATE;
+
+public class OrderDate {
+
+    private static final int MIN_DAY = 1;
+    private static final int NAX_DAY = 31;
+
+    private final int day;
+
+    public OrderDate(final int day) {
+        validate(day);
+        this.day = day;
+    }
+
+    private void validate(final int day) {
+        if (day < MIN_DAY || day > NAX_DAY) {
+            throw new IllegalArgumentException(INVALID_DATE.getMessage());
+        }
+    }
+
+    public static OrderDate withDay(final int day) {
+        return new OrderDate(day);
+    }
+}
