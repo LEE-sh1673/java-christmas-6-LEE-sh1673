d3e53095f067e6d4ff761c56b90029b92f9d93f8
LEE-sh1673 <lsh901673@gmail.com>
2023-11-15 19:05:32 +0900

    test: 월의 날짜가 주말인지 판별하는 기능 테스트 추가
    - 월의 날짜가 금요일과 토요일이 아니면 평일이다.
    - 월의 날짜가 금요일 혹은 토요일이면 주말이다.

--- src/test/java/christmas/model/event/EventCalendarTest.java
+++ src/test/java/christmas/model/event/EventCalendarTest.java
@@ -1,10 +1,13 @@
 package christmas.model.event;
 
 import static christmas.model.event.EventFixture.createDecemberCalendar;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.exception.ErrorType;
+import java.time.MonthDay;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -23,5 +26,19 @@ class EventCalendarTest {
         assertThatThrownBy(() -> calendar.isSpecialDay(dayOfMonth))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorType.INVALID_DATE.getMessage());
+    }
+
+    @DisplayName("월의 날짜가 금요일 혹은 토요일이면 주말이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30})
+    void givenFridayOrSaturDay_Then_TrueReturns(final int dayOfMonth) {
+        assertThat(calendar.isWeekend(dayOfMonth)).isTrue();
+    }
+
+    @DisplayName("월의 날짜가 금요일과 토요일이 아니면 평일이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31})
+    void givenDayOfMonthNotFridayAndSaturday_Then_FalseReturns(final int dayOfMonth) {
+        assertThat(calendar.isWeekend(dayOfMonth)).isFalse();
     }
 }
