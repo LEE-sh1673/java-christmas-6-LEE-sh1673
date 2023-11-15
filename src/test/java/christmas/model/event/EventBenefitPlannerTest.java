5f1297e2277f6cc8c8c70779d6a91089b7961859
LEE-sh1673 <lsh901673@gmail.com>
2023-11-15 19:55:51 +0900

    refactor: EventBenefitPlannerTest에 대한 리팩토링 진행
    변경 내용:
    - EventBenefitPlanner를 생성하는 테스트 픽스처 추가
    - 중복되어 사용되는 객체를 상수화
    - 중복된 테스트 케이스 삭제

--- src/test/java/christmas/model/event/EventBenefitPlannerTest.java
+++ src/test/java/christmas/model/event/EventBenefitPlannerTest.java
@@ -1,13 +1,12 @@
 package christmas.model.event;
 
 import static christmas.model.OrderFixture.createOrder;
-import static christmas.model.event.EventFixture.createDecemberCalendar;
+import static christmas.model.event.EventFixture.createDecemberPlanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import christmas.model.GiftMenus;
 import christmas.model.Order;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,17 +14,12 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 class EventBenefitPlannerTest {
 
-    private static final EventBenefitPlanner planner = new EventBenefitPlanner();
-    private static final EventCalendar calendar = createDecemberCalendar();
+    private static final EventBenefitPlanner planner = createDecemberPlanner();
 
-    @BeforeAll
-    static void setUp() {
-        planner.add(new DdayDiscountEvent());
-        planner.add(new WeekdayDiscountEvent(calendar));
-        planner.add(new WeekendDiscountEvent(calendar));
-        planner.add(new SpecialDayDiscountEvent(calendar));
-        planner.add(new GiftMenuDiscountEvent());
-    }
+    private static final Order BASIC_ORDER = createOrder(
+            "3",
+            "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
+    );
 
     @DisplayName("총 주문 금액이 10,000원 미만일 때 이벤트를 적용하지 않는다.")
     @Test
@@ -57,35 +51,8 @@ class EventBenefitPlannerTest {
     @DisplayName("주어진 메뉴를 주문했을 때 총 혜택 금액을 구한다.")
     @Test
     void givenOrderMenus_Then_TotalBenefitPrizeReturns() {
-        // given
-        final Order order = createOrder("3",
-                "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
-        );
-
-        // when
-        final EventBenefits benefits = planner.plan(order);
-
-        // then
+        final EventBenefits benefits = planner.plan(BASIC_ORDER);
         assertThat(benefits.calculateTotalPrize()).isEqualTo(31246L);
-    }
-
-    @DisplayName("크리스마스에 아이스크림 2개를 주문하면 예상 결제 금액은 1,554원이다.")
-    @Test
-    void givenChristmasWithIceCream_Then_BenefitsReturn() {
-        // given
-        final Order order = createOrder("25", "아이스크림-2");
-        final GiftMenus giftMenus = GiftMenus.from(order);
-
-        // when
-        final EventBenefits benefits = planner.plan(order);
-
-        // then
-        final long totalBenefitPrize = benefits.calculateTotalPrize();
-        final long estimatedPaymentPrize
-                = benefits.calculateEstimatedPaymentPrize(order, giftMenus);
-
-        assertThat(totalBenefitPrize).isEqualTo(8446L);
-        assertThat(estimatedPaymentPrize).isEqualTo(1554L);
     }
 
     @DisplayName("할인 금액이 없으면 예상 결제 금액은 할인 전 총주문 금액이다.")
@@ -108,15 +75,12 @@ class EventBenefitPlannerTest {
     @Test
     void givenOrderMenus_Then_EstimatedPaymentPrizeReturns() {
         // given
-        final Order order = createOrder("3",
-                "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
-        );
-        final GiftMenus giftMenus = GiftMenus.from(order);
+        final GiftMenus giftMenus = GiftMenus.from(BASIC_ORDER);
 
         // when
-        final EventBenefits benefits = planner.plan(order);
+        final EventBenefits benefits = planner.plan(BASIC_ORDER);
         final long estimatedPaymentPrize
-                = benefits.calculateEstimatedPaymentPrize(order, giftMenus);
+                = benefits.calculateEstimatedPaymentPrize(BASIC_ORDER, giftMenus);
 
         // then
         assertThat(estimatedPaymentPrize).isEqualTo(135754L);
@@ -125,15 +89,7 @@ class EventBenefitPlannerTest {
     @DisplayName("주어진 메뉴를 주문했을 때 발급되는 배지를 구한다.")
     @Test
     void givenOrderMenus_Then_EventBadgeReturns() {
-        // given
-        final Order order = createOrder("3",
-                "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
-        );
-
-        // when
-        final EventBenefits benefits = planner.plan(order);
-
-        // then
+        final EventBenefits benefits = planner.plan(BASIC_ORDER);
         assertThat(benefits.publishBadge()).isEqualTo(EventBadge.SANTA);
     }
 
