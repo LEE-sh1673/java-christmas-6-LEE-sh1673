3f355465ffa03f337127beea006f946bfecac5d5
LEE-sh1673 <lsh901673@gmail.com>
2023-11-15 20:35:08 +0900

    refactor: OutputView 클래스에 대한 리팩토링 적용
    OutputView 내부에서 도메인 로직을 호출하는 대신 컨트롤러에서 계산하여 UI와 도메인 로직을 분리하도록 구조를 수정하였습니다.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,10 +2,9 @@ package christmas.view;
 
 import christmas.model.GiftMenu;
 import christmas.model.GiftMenus;
-import christmas.model.Order;
 import christmas.model.OrderMenu;
 import christmas.model.event.EventBadge;
-import christmas.model.event.EventBenefits;
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
@@ -47,52 +46,56 @@ public class OutputView {
     }
 
     public static void printMenu(
-            final Order order,
-            final GiftMenus giftMenus,
-            final EventBenefits benefits
+            final int orderDay,
+            final List<OrderMenu> orderMenus,
+            final long totalOrderPrize
     ) {
-        printMenuTitle(order);
-        printOrderMenus(order);
-        printTotalOrderPrice(order);
-
-        printGiftMenus(giftMenus.getMenus());
-        printBenefits(benefits.getBenefits());
-
-        printTotalBenefitPrize(benefits);
-        printEstimatedPaymentPrize(benefits, order, giftMenus);
-        printEventBadge(benefits);
+        printMenuTitle(orderDay);
+        printOrderMenus(orderMenus);
+        printTotalOrderPrice(totalOrderPrize);
     }
 
-    private static void printMenuTitle(final Order order) {
-        System.out.printf((TOTAL_EVENT_BENEFITS_TITLE) + "%n", order.getDay());
+    private static void printMenuTitle(final int orderDay) {
+        System.out.printf((TOTAL_EVENT_BENEFITS_TITLE) + "%n", orderDay);
     }
 
-    private static void printOrderMenus(final Order order) {
+    private static void printOrderMenus(final List<OrderMenu> orderMenus) {
         System.out.println(System.lineSeparator() + ORDER_MENUS_TITLE);
 
-        for (final OrderMenu menu : order.getMenus()) {
+        for (final OrderMenu menu : orderMenus) {
             System.out.printf((ORDER_MENU_FORMAT) + "%n", menu.getName(), menu.getQuantity());
         }
     }
 
-    private static void printTotalOrderPrice(final Order order) {
+    private static void printTotalOrderPrice(final long totalOrderPrize) {
         System.out.println(System.lineSeparator() + TOTAL_ORDER_PRIZE_TITLE);
-        System.out.printf((PRICE_FORMAT) + "%n", order.calculateTotalPrize());
+        System.out.printf((PRICE_FORMAT) + "%n", totalOrderPrize);
     }
 
-    private static void printGiftMenus(final Map<GiftMenu, Long> giftMenus) {
+    public static void printGiftMenus(final GiftMenus giftMenus) {
+        final Map<GiftMenu, Long> menuDetails = giftMenus.getMenus();
         System.out.println(System.lineSeparator() + GIFT_MENUS_TITLE);
 
-        if (giftMenus.isEmpty()) {
+        if (menuDetails.isEmpty()) {
             System.out.println(NON_EXISTS);
             return;
         }
-        for (final GiftMenu menu : giftMenus.keySet()) {
-            System.out.printf((ORDER_MENU_FORMAT) + "%n", menu.getName(), giftMenus.get(menu));
+        for (final GiftMenu menu : menuDetails.keySet()) {
+            System.out.printf((ORDER_MENU_FORMAT) + "%n", menu.getName(), menuDetails.get(menu));
         }
     }
 
-    private static void printBenefits(final Map<String, Long> benefits) {
+    public static void printBenefits(
+            final Map<String, Long> details,
+            final long totalBenefitPrize,
+            final long estimatedPaymentPrize
+    ) {
+        printBenefitDetails(details);
+        printTotalBenefitPrize(totalBenefitPrize);
+        printEstimatedPaymentPrize(estimatedPaymentPrize);
+    }
+
+    private static void printBenefitDetails(final Map<String, Long> benefits) {
         System.out.println(System.lineSeparator() + BENEFITS_ITLE);
 
         if (benefits.isEmpty()) {
@@ -104,8 +107,7 @@ public class OutputView {
         }
     }
 
-    private static void printTotalBenefitPrize(final EventBenefits benefits) {
-        final long totalBenefitPrize = benefits.calculateTotalPrize();
+    private static void printTotalBenefitPrize(final long totalBenefitPrize) {
         System.out.println(System.lineSeparator() + TOTAL_BENEFIT_PRIZE_TITLE);
 
         if (totalBenefitPrize == 0L) {
@@ -115,18 +117,12 @@ public class OutputView {
         System.out.printf((TOTAL_BENEFIT_PRIZE_FORMAT) + "%n", totalBenefitPrize);
     }
 
-    private static void printEstimatedPaymentPrize(
-            final EventBenefits benefits,
-            final Order order,
-            final GiftMenus giftMenus
-    ) {
-        long estimatedPaymentPrize = benefits.calculateEstimatedPaymentPrize(order, giftMenus);
+    private static void printEstimatedPaymentPrize(final long estimatedPaymentPrize) {
         System.out.println(System.lineSeparator() + ESTIMATED_PAYMENT_PRIZE_TITLE);
         System.out.printf((PRICE_FORMAT) + "%n", estimatedPaymentPrize);
     }
 
-    private static void printEventBadge(final EventBenefits benefits) {
-        final EventBadge badge = benefits.publishBadge();
+    public static void printEventBadge(final EventBadge badge) {
         System.out.println(System.lineSeparator() + EVENT_BADGE_TITLE);
         System.out.printf(badge.getName());
     }
