61e19ce3a3cf84b4855b6b1baae41c320709ce19
LEE-sh1673 <lsh901673@gmail.com>
2023-12-24 23:49:00 +0900

    test: 주문 메뉴의 수량을 검증하는 테스트 추가
    - 주문 메뉴의 수량이 20개를 초과하면 예외가 발생한다.

--- src/test/java/christmas/model/OrderMenusTest.java
+++ src/test/java/christmas/model/OrderMenusTest.java
@@ -3,20 +3,26 @@ package christmas.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.exception.OrderException;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderMenusTest {
 
+    private static OrderMenus createMenus(final String menu) {
+        return new OrderMenus(OrderMenusFactory.createMenus(menu));
+    }
 
-    //TODO: 음료 주문, 메뉴의 총 수량에 대한 검증 테스트 추가할 것
     @DisplayName("음료만 주문하는 경우 예외가 발생한다.")
     @Test
     void givenMenus_WhenAllBeverage_ThenExceptionOccurs() {
-        List<OrderMenu> menus = OrderMenusFactory.createMenus("제로콜라-2,레드와인-10");
+        assertThatThrownBy(() -> createMenus("제로콜라-2,레드와인-10"))
+                .isInstanceOf(OrderException.class);
+    }
 
-        assertThatThrownBy(() -> new OrderMenus(menus))
+    @DisplayName("주문 메뉴의 수량이 20개를 초과하면 예외가 발생한다.")
+    @Test
+    void givenTotalQuantity_WhenOutOfRange_ThenExceptionOccurs() {
+        assertThatThrownBy(() -> createMenus("바비큐립-4,초코케이크-2,제로콜라-11,초코케이크-4"))
                 .isInstanceOf(OrderException.class);
     }
 }
